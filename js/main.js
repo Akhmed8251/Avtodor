c8dd81c393e832f70407a2af44b781910b03c0c3
Akhmed8251 <bratya_250@mail.ru>
2023-11-13 12:47:12 +0300

    доделал десктоп версию главной страницы

--- js/main.js
+++ js/main.js
@@ -1,4 +1,33 @@
 document.addEventListener("DOMContentLoaded", function () {
+  const headerMenu = document.querySelector(".header-menu")
+  const menuBtn = document.querySelector(".header-bottom__menu-btn")
+  menuBtn.addEventListener("click", function() {
+    headerMenu.classList.toggle("_active")
+    menuBtn.classList.toggle("_close")
+
+    if (headerMenu.classList.contains("_active")) {
+      document.body.classList.add("no-scroll")
+      document.body.classList.add("backdrop")
+    } else {
+      document.body.classList.remove("no-scroll")
+      document.body.classList.remove("backdrop")
+    }
+  })
+
+  const menuItemsWithSubmenu = document.querySelectorAll(".has-submenu");
+  menuItemsWithSubmenu.forEach((menuItem) => {
+    menuItem.addEventListener("click", () => {
+      menuItem.classList.toggle("open");
+
+      let submenu = menuItem.querySelector(".submenu");
+      if (menuItem.classList.contains("open")) {
+        submenu.style.maxHeight = submenu.scrollHeight + "px";
+      } else {
+        submenu.style.maxHeight = null;
+      }
+    });
+  });
+
   new Swiper(".hero__slider", {
     allowTouchMove: true,
     navigation: {
@@ -13,39 +42,46 @@ document.addEventListener("DOMContentLoaded", function () {
   new Swiper(".news__slider", {
     spaceBetween: 30,
     breakpoints: {
-        320: {
-            slidesPerView: 1.25,
-        },
-        526: {
-           slidesPerView: 2,
-        },
-        993: {
-            slidesPerView: 3
-        }
-    }
-  })
+      320: {
+        slidesPerView: 1.25,
+      },
+      526: {
+        slidesPerView: 2,
+      },
+      993: {
+        slidesPerView: 3,
+      },
+    },
+  });
 
   new Swiper(".partners__slider", {
     slidesPerView: 4,
     spaceBetween: 35,
     pagination: {
-        el: ".partners .swiper-pagination",
-    }
-  })
+      el: ".partners .swiper-pagination",
+    },
+  });
 
   ymaps.ready(function () {
-    var myMap = new ymaps.Map('map', {
-            center: [42.982105543873985,47.46713613558195],
-            zoom: 17
-        }, {
-            searchControlProvider: 'yandex#search'
-        })
+    var myMap = new ymaps.Map(
+      "map",
+      {
+        center: [42.982105543873985, 47.46713613558195],
+        zoom: 17,
+      },
+      {
+        searchControlProvider: "yandex#search",
+      }
+    );
 
-        let myPlacemark = new ymaps.Placemark([42.982105543873985,47.46713613558195], {
-            iconLayout: "default#image",
-            iconImageSize: [36, 44]
-        })
+    let myPlacemark = new ymaps.Placemark(
+      [42.982105543873985, 47.46713613558195],
+      {
+        iconLayout: "default#image",
+        iconImageSize: [36, 44],
+      }
+    );
 
-    myMap.geoObjects.add(myPlacemark)
+    myMap.geoObjects.add(myPlacemark);
   });
 });
